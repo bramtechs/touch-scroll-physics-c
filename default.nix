e42f81c4f0239c7877782234b0a86a9c6425d198
bramtechs <bramtech@telenet.be>
2024-08-05 16:52:05 +0200

    nix pkg files

---
+++ default.nix
@@ -0,0 +1,29 @@
+{ 
+  pkgs ? import <nixpkgs> {}
+}:
+
+with pkgs;
+
+pkgs.stdenv.mkDerivation (finalAttrs: {
+  pname = "touch-scroll-physics-c";
+  version = "0.0.1";
+
+  src = ./.;
+
+  outputs = [ "out" ];
+  nativeBuildInputs = [ cmake ninja ];
+  enableParallelBuilding = true;
+
+  configurePhase = ''
+    cmake . -G Ninja
+  '';
+
+  meta = with lib; {
+    description = "Quick port of a JavaScript scroll library to C.";
+    homepage = "https://github.com/bramtechs/touch-scroll-physics-c";
+    license = licenses.mit;
+    maintainers = with maintainers; [ brambasiel ];
+    platforms = platforms.all;
+    changelog = "https://github.com/bramtechs/touch-scroll-physics-c";
+  };
+})
